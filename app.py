3e612fbc5624128adf149cf578f0e62a086cc4ee
GirishaDaggula <girishadaggula@gmail.com>
2025-03-07 13:55:55 -0500

    Assignment 3

---
+++ app.py
@@ -0,0 +1,5 @@
+def calculate_area(side_length):
+    """
+    Function to calculate the area of a square.
+    """
+    return side_length ** 2
---
+++ test_app.py
@@ -0,0 +1,5 @@
+from app import calculate_area
+
+def test_area_with_student_id():
+    assert calculate_area(XX) == 33**2  # Replace XX with last two digits of your student ID
+
