8d63d8c491890ba2f616783ae8b53300c780403e
Girisha Daggula <girisha.daggula@dcmail.ca>
2025-03-07 15:17:50 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,12 @@
-from app import calculate_area
+from app import calculate_area  
+
+def test_area():
+    assert calculate_area(5) == 25  
+
 
 def test_area_with_student_id():
-    assert calculate_area(33) == 33**2  
+    assert calculate_area(33) == 33 ** 2  
+
+def test_area_failure():
+    assert calculate_area(5) == 30  
 
